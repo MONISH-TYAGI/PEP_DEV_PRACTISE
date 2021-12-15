b3f3ce14ed822b1153f379bd24aecdd2b5480610
MONISH-TYAGI <monishtyagi5770@gmail.com>
2021-12-15 22:37:17 +0530

    Practise

--- CliResume.js
+++ CliResume.js
@@ -1,52 +1,58 @@
 var inquirer = require('inquirer');
-const cp=require('child_process')
+const cp = require('child_process');
+
+
 function displayDetails(){
 inquirer
   .prompt([
-   { /* Pass your questions in here */
-    type:'list',
-    name:'Selection',
-    choices:["About","Skills","Projects","Academics"]
-}
+    {
+           type:'list',
+           name :'Selection',
+           choices:['About' , 'Skills' , 'Projects' , 'Academics']
+    }
   ])
   .then((choices) => {
-      if(choices.Selection=='About')
-      {
-          console.log(`Candidate Name - Monish Tyagi`)
-          displayNext()
-      }
-      else if(choices.Selection=='Skills')
-      {
-          console.log('java,js,c++,c')
-          displayNext()
-      }
-      else if(choices.Selection=='Projects')
-      {
-          cp.execSync('start chrome https://github.com/MONISH-TYAGI')
-          displayNext()
-      }
-      else if(choices.Selection=='Academics')
-      {
-          cp.execSync('start chrome https://www.linkedin.com/login')
-          displayNext()
-      }
+       if(choices.Selection =='About'){
+              console.log(`Candidate -Monish Tyagi 
+              2nd year student 
+              BPIT College (GGSIPU)`);
+              displayNext()
+       }
+
+       else if(choices.Selection=='Skills'){
+              console.log('JavaScript , Node , React , GoLang , Java ')
+              displayNext()
+       }
+
+       else if(choices.Selection=='Projects'){
+            cp.execSync('start chrome https://github.com/MONISH-TYAGI')
+            displayNext()
+       }
+
+       else if(choices.Selection=='Academics'){
+              cp.execSync('start chrome http://www.linkedin.com/in/monish-tyagi-ab589a202')
+              displayNext()
+       }
   })
+ 
 }
+
 displayDetails()
+
+
 function displayNext(){
-var inquirer = require('inquirer');
+       var inquirer = require('inquirer');
 inquirer
   .prompt([
-    {
-        type:'list',
-        name:'GoBack',
-        choices:['black']
-    }
+         { type:'list',
+       name :'GoBack',
+       choices:['back']
+         }
   ])
   .then((choice) => {
-    if(choice.GoBack='back')
-    {
-        displayDetails()
+    if(choice.GoBack = 'back'){
+           displayDetails()
     }
   })
+ 
 }
